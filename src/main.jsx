bf121497d9942b6b211f3438defdd1dce89deb28
grantdozier <gdozier110@gmail.com>
2026-02-20 16:24:49 -0600

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,13 @@ import './index.css'
 import ScrollToTop from './components/ScrollToTop'
 import { CartProvider } from './context/CartContext'
 
+// Fix Safari back-forward cache (bfcache) glitch
+window.addEventListener('pageshow', (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
